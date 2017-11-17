bd22b02095ae731d36682e56cd16138dc5114f7a
dmaddox <dmaddox@gmail.com>
2017-11-17 13:53:31 -0600

    updating ajax query

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,14 +3,24 @@
 
 //Search button functionality
 $("#search_button_holder").on("click", function() {
-	//run api query function
+	//build the api query 
 	var searchTerm = $(this).attr("");
     var beginDate = $(this).attr("");
     var endDate = $(this).attr("");
     var page = $(this).attr("");
 
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=7cc52f271251486f990d42d1dfc60c68&q="+searchTerm+"&begin_date="+beginDate+"&enddate="+endDate+"&page="page;
+	
+	//call the query
+	$.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+      	console.log();
+      }
+
 	//run displayResults function
+
 	displayResults();
 });
 
@@ -24,7 +34,12 @@ $("#clear_button_holder").on("click", function() {
 
 //displayResults function
 function displayResults() {
-	//create results var
+
+	//main (title)
+	//section
+	//article date
+	//source URL
+
 	//loop through each result & build the result[i] div
 	//display results
 	$("#results").prepend(place_holder);
